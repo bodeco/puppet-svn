19193dd612ea797a8bb71b54d02f44faeeb4b42c
Nan Liu <nan.liu@gmail.com>
2014-10-07 15:50:24 -0700

    Remove ensure since it requires newer version of puppet.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 class svn {
   yumrepo { 'WandiscoSVN':
-    ensure   => present,
     baseurl  => 'http://opensource.wandisco.com/centos/6/svn-1.8/RPMS/$basearch/',
     enabled  => true,
     gpgcheck => false,
